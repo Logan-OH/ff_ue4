c6b5fab216e77c584843886406649a4bce652273
Logan_OH <35443660+Logan-OH@users.noreply.github.com>
2026-09-17 16:15:55 -0500

    Add Crown Circuit Unreal module rules

---
+++ CrownCircuit/Source/CrownCircuit/CrownCircuit.Build.cs
@@ -0,0 +1,23 @@
+using UnrealBuildTool;
+
+public class CrownCircuit : ModuleRules
+{
+    public CrownCircuit(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "UMG",
+            "Slate",
+            "SlateCore",
+            "HTTP",
+            "Json",
+            "JsonUtilities"
+        });
+    }
+}
