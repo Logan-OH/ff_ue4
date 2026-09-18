957600d8c151ac02a153b6097296cab9f4dcfb02
Logan_OH <35443660+Logan-OH@users.noreply.github.com>
2026-09-17 21:37:48 -0500

    Use CurrentFocusLane backing property

--- CrownCircuit/Source/CrownCircuit/Private/CCOperationDirector.cpp
+++ CrownCircuit/Source/CrownCircuit/Private/CCOperationDirector.cpp
@@ -7,7 +7,7 @@ ACCOperationDirector::ACCOperationDirector()
 
 void ACCOperationDirector::FocusLane(FName LaneId)
 {
-    FocusLane = LaneId;
+    CurrentFocusLane = LaneId;
     OnLaneFocusChanged.Broadcast(LaneId);
     if (LaneId == TEXT("global")) BP_ReturnToOverview();
     else BP_FocusLane(LaneId);
